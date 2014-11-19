9207ef1fec775d49d01a87fc6e3fce290a72027c
Camilo Aguilar <camilo.aguilar@gmail.com>
2014-11-19 12:06:00 -0500

    Golint fix

--- licentia_test.go
+++ licentia_test.go
@@ -14,7 +14,7 @@ import (
 	"testing"
 )
 
-var mpl2 string = `// This Source Code Form is subject to the terms of the Mozilla Public
+var mpl2 = `// This Source Code Form is subject to the terms of the Mozilla Public
 // License, version 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
